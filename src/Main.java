1c08eb948e4a48431c3173019c3d51d12e7a72fb
CArefiev <cristina.arefiev@endava.com>
2020-10-30 07:20:58 +0200

    New changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
+
     public static void sum (int a, int b) {
         System.out.println(a+b);
     }
@@ -7,9 +8,23 @@ public class Main {
     public static void prod (int a, int b) {
         System.out.println(a*b);
     }
+
+
+    public static Double factorial(Integer a) {
+
+        double fact = 1;
+        for (int i = 1; i <= a; i++) {
+
+            fact = fact * i;
+        }
+        return fact;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
         sum(10,20);
         prod(10,20);
+
+        System.out.println(factorial(5));
+
     }
 }
