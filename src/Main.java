8316bda626902a0919be6bdad4fb96607e50ee1d
CArefiev <cristina.arefiev@endava.com>
2020-10-30 06:52:30 +0200

    Added one method to Branch2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
+    public static Double factorial(Integer a) {
+
+        double fact = 1;
+        for (int i = 1; i <= a; i++) {
+
+            fact = fact * i;
+        }
+        return fact;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(factorial(5));
     }
 }
